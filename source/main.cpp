15f13c972a672807485453a57f5906adaddc42cb
jan everaert <jan@crashlab.be>
2023-01-09 17:29:39 +0100

    final working SD and stuff'

--- source/main.cpp
+++ source/main.cpp
@@ -2,90 +2,86 @@
 #include <stdio.h>
 #include "f_util.h"
 #include "ff.h"
+
 #include "pico/stdlib.h"
+#include "pico/stdlib.h"
+#include "pico/util/datetime.h"
 #include "pico/analog_microphone.h"
+#include <pico/stdio_usb.h>
+
+
 #include "tusb.h"
 #include "rtc.h"
 #include "sd_card.h"
+#include <string> 
 
 #include <hardware/gpio.h>
 #include <hardware/uart.h>
-
-#include <pico/stdio_usb.h>
-
+#include "hardware/rtc.h"
 extern "C" void writ();
 
-void writ() {
 
-    FRESULT fr;
-    FATFS fs;
-    FIL fil;
-    int ret;
-    char buf[100];
-    char filename[] = "test02.txt";
+
+FRESULT fr;
+FATFS fs;
+FIL fil;
+int ret;
+char buf[100];
+char filename[] = "INFERENCE.CSV";
+
+
+void writeToSD(std::string in, bool separate, bool endLine) {
+    if (separate) {
+      in += ",";
+    }
+    if (endLine) {
+      in += "\r\n";
+    }
+    
+    std::basic_string<TCHAR> converted(in.begin(), in.end());
+
+    const TCHAR *tchar = converted.c_str();
+
+    // Write something to file
+
+    ret = f_printf(&fil, tchar);
+
+    if (ret < 0) {
+        printf("ERROR: Could not write to file (%d)\r\n", ret);
+        f_close(&fil);
+    } else {
+      printf("       w(%d) \n", ret);
+    }
+
+}
+
+void initSD() {
 
 
     // Initialize SD card
     if (!sd_init_driver()) {
         printf("ERROR: Could not initialize SD card\r\n");
-        while (true);
     }
 
     // Mount drive
     fr = f_mount(&fs, "0:", 1);
     if (fr != FR_OK) {
         printf("ERROR: Could not mount filesystem (%d)\r\n", fr);
-        while (true);
     }
 
     // Open file for writing ()
-    fr = f_open(&fil, filename, FA_WRITE | FA_CREATE_ALWAYS);
+    fr = f_open(&fil, filename, FA_OPEN_APPEND | FA_WRITE);
     if (fr != FR_OK) {
         printf("ERROR: Could not open file (%d)\r\n", fr);
-        while (true);
     }
+}
 
-    // Write something to file
-    ret = f_printf(&fil, "This is another test\r\n");
-    if (ret < 0) {
-        printf("ERROR: Could not write to file (%d)\r\n", ret);
-        f_close(&fil);
-        while (true);
-    }
-    ret = f_printf(&fil, "of writing to an SD card.\r\n");
-    if (ret < 0) {
-        printf("ERROR: Could not write to file (%d)\r\n", ret);
-        f_close(&fil);
-        while (true);
-    }
+void closeSD() {
 
     // Close file
     fr = f_close(&fil);
     if (fr != FR_OK) {
         printf("ERROR: Could not close file (%d)\r\n", fr);
-        while (true);
-    }
-
-    // Open file for reading
-    fr = f_open(&fil, filename, FA_READ);
-    if (fr != FR_OK) {
-        printf("ERROR: Could not open file (%d)\r\n", fr);
-        while (true);
-    }
-
-    // Print every line in file over serial
-    printf("Reading from file '%s':\r\n", filename);
-    printf("---\r\n");
-    while (f_gets(buf, sizeof(buf), &fil)) {
-        printf(buf);
-    }
-    printf("\r\n---\r\n");
-
-    // Close file
-    fr = f_close(&fil);
-    if (fr != FR_OK) {
-        printf("ERROR: Could not close file (%d)\r\n", fr);
-        while (true);
     }
 
     // Unmount drive
@@ -142,48 +138,66 @@ int main( void )
     stdio_init_all();
 
 
+    char datetime_buf[256];
+    char *datetime_str = &datetime_buf[0];
 
-    // while (!tud_cdc_connected()) {
-    //     tight_loop_contents();
-    // }
+    // Start on Friday 5th of June 2020 15:45:00
+    datetime_t t = {
+            .year  = 2023,
+            .month = 01,
+            .day   = 9,
+            .dotw  = 1, // 0 is Sunday, so 5 is Friday
+            .hour  = 15,
+            .min   = 45,
+            .sec   = 00
+    };
 
-    // printf("hello analog microphone\n");
+    // Start the RTC
+    rtc_init();
+    rtc_set_datetime(&t);
 
-    // // // initialize the analog microphone
-    // if (analog_microphone_init(&config) < 0) {
-    //     printf("analog microphone initialization failed!\n");
-    //     while (1) { tight_loop_contents(); }
-    // }
-    // printf("after analog microphone\n");
-    // ei_sleep(1000);
+    while (!tud_cdc_connected()) {
+        tight_loop_contents();
+    }
+
+    printf("hello analog microphone\n");
+
+    // initialize the analog microphone
+    if (analog_microphone_init(&config) < 0) {
+        printf("analog microphone initialization failed!\n");
+        while (1) { tight_loop_contents(); }
+    }
+    printf("after analog microphone\n");
+    ei_sleep(1000);
 
 
-    // // set callback that is called whanalogen all the samples in the library
-    // // internal sample buffer are ready for reading
-    // analog_microphone_set_samples_ready_handler(on_analog_samples_ready);
-    // printf("after sample\n");
+    // set callback that is called whanalogen all the samples in the library
+    // internal sample buffer are ready for reading
+    analog_microphone_set_samples_ready_handler(on_analog_samples_ready);
+    printf("after sample\n");
     
-    // // start capturing data from the analog microphone
-    // if (analog_microphone_start() < 0) {
-    //     printf("PDM microphone start failed!\n");
-    //     while (1) { tight_loop_contents();  }
-    // }    
+
+    // printf("result of microphone: %d \n", analog_microphone_start());
+    
+    if (analog_microphone_start() < 0) {
+        printf("PDM microphone start failed!\n");
+        // while (1) { tight_loop_contents();  }
+    }    
+
+    initSD();
+    writeToSD("time,timingDSP-classification-anomaly,c1,c2,c3,", false, true);
+    closeSD();
 
     while (true) {
-      printf("going into writing \n");
 
-      writ();
-      sleep_ms(1000);
-        
-
-
+        rtc_get_datetime(&t);
         if(inferencing) {
 
 
             printf("Edge Impulse standalone inferencing (Raspberry Pi Pico)\n");
             printf("finding: %d\n", sizeof(features));
             printf("\n");
-
+            initSD();
             if (sizeof(features) / sizeof(float) != EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE) {
                 ei_printf("The size of your 'features' array is not correct. Expected %d items, but had %u\n", EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, sizeof(features) / sizeof(float));
                 // inferencing = false;
@@ -214,36 +228,35 @@ int main( void )
                     printf("error\n");
                 } else {
 
+                  // if (FR_OK != fr && FR_EXIST != fr) {
+                  //   panic("f_open(%s) error: %s (%d)\n", filename, FRESULT_str(fr), fr);
+                  // }
+
+
+                  datetime_to_str(datetime_str, sizeof(datetime_buf), &t);
+                  printf("\r%s      ", datetime_str);
+                  writeToSD( datetime_str, false, false);
+                  writeToSD( "," + std::to_string(result.timing.dsp)+"-"+std::to_string(result.timing.classification)+"-"+std::to_string(result.timing.anomaly) + ",", false, false);
+                  
+
                   printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n", result.timing.dsp, result.timing.classification, result.timing.anomaly);
 
                   // print the predictions
                   printf("[");
                  
-                  // if (FR_OK != fr && FR_EXIST != fr) {
-                  //   panic("f_open(%s) error: %s (%d)\n", filename, FRESULT_str(fr), fr);
-                  // }
-                  
+
                   for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++)
                   {
                       ei_printf("%.5f", result.classification[ix].value);
+                                          
+                      writeToSD( std::to_string(result.classification[ix].value), true, false);
 
-                      
-
-                      // if (f_printf(&fil, "%f", result.classification[ix].value) < 0) {
-                      //   printf("f_printf failed\n");
-                      // }     
                       if(EI_CLASSIFIER_HAS_ANOMALY == 1) {
                         ei_printf(", ");
-                        // if (f_printf(&fil, ",") < 0) {
-                        //   printf("f_printf failed\n");
-                        // }     
                       } else {
                         if (ix != EI_CLASSIFIER_LABEL_COUNT - 1)
                         {
                           ei_printf(", ");
-                          // if (f_printf(&fil, ",") < 0) {
-                          //   printf("f_printf failed\n");
-                          // }     
                         }
                     }
 
@@ -256,8 +269,9 @@ int main( void )
 
                 printf("]\n");
             }   
-
+            writeToSD("", false, true); 
             ei_sleep(2000);
+            closeSD();
           
             inferencing = false;
             writing_on = 0;
@@ -283,5 +297,5 @@ int main( void )
         }
     }
 
-    // return 0;
+    return 0;
 }
